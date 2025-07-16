e5fd9c36982528b9cf88f6e6f1d11316dd712381
Parvez141300 <parvezhossain744471@gmail.com>
2025-07-16 14:20:22 +0600

    added banner section a onfocus and onblur a bg-color

--- src/pages/Home/shared/Banner.jsx
+++ src/pages/Home/shared/Banner.jsx
@@ -1,40 +1,60 @@
-import React from 'react';
-import { FaSearch } from 'react-icons/fa';
-import bannerImg from '../../../assets/banner_image/banner_image.png'
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import bannerImg from "../../../assets/banner_image/banner_image.png";
 
 const Banner = () => {
+  const [isFocused, setIsFocused] = useState(false);
+
+
   return (
     <div
-      className="relative bg-cover bg-center min-h-[70vh] lg:min-h-screen flex items-center justify-center text-white px-4"
+      className="relative bg-cover bg-center min-h-[70vh] lg:min-h-screen "
       style={{
         backgroundImage: `url(${bannerImg})`,
       }}
     >
-      <div className="max-w-2xl w-full text-center space-y-6">
-        <h1 className="text-3xl md:text-4xl font-bold">Search Your Desired Post By Tag</h1>
-        <p className="text-lg md:text-xl">
-          Search here to get answers to your questions
-        </p>
+      <div className={`absolute inset-0  ${isFocused ? 'bg-black/60' : 'bg-black/45'} flex items-center justify-center text-white px-4`}>
+        <div className="max-w-2xl w-full text-center space-y-6">
+          <h1 className="text-3xl md:text-4xl font-bold">
+            Search Your Desired Post By Tag
+          </h1>
+          <p className="text-lg md:text-xl">
+            Search here to get answers to your questions
+          </p>
 
-        {/* Search Bar */}
-        <div className="relative mt-6">
-          <input
-            type="text"
-            placeholder="Search your desired post..."
-            className="input input-bordered w-full rounded-full px-5 py-7 text-black focus:outline-0"
-          />
-          <button className="absolute z-50 top-2 right-3 btn btn-primary rounded-full">
-            <FaSearch /> Search
-          </button>
-        </div>
+          {/* Search Bar */}
+          <div className="relative mt-6">
+            <input
+              type="text"
+              placeholder="Search your desired post..."
+              className="input input-bordered w-full rounded-full px-5 py-7 text-black focus:outline-0"
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+            />
+            <button className="absolute z-50 top-2 right-3 btn btn-primary rounded-full">
+              <FaSearch /> Search
+            </button>
+          </div>
 
-        {/* Popular Keywords */}
-        <div className="text-sm mt-2">
-          <span className="font-semibold">Popular:</span>{' '}
-          <span className="link link-hover text-gray-200 hover:text-white">Code</span>,{' '}
-          <span className="link link-hover text-gray-200 hover:text-white">Basic</span>,{' '}
-          <span className="link link-hover text-gray-200 hover:text-white">Blog</span>,{' '}
-          <span className="link link-hover text-gray-200 hover:text-white">WordPress</span>
+          {/* Popular Keywords */}
+          <div className="text-sm mt-2">
+            <span className="font-semibold">Popular:</span>{" "}
+            <span className="link link-hover text-gray-200 hover:text-white">
+              Code
+            </span>
+            ,{" "}
+            <span className="link link-hover text-gray-200 hover:text-white">
+              Basic
+            </span>
+            ,{" "}
+            <span className="link link-hover text-gray-200 hover:text-white">
+              Blog
+            </span>
+            ,{" "}
+            <span className="link link-hover text-gray-200 hover:text-white">
+              WordPress
+            </span>
+          </div>
         </div>
       </div>
     </div>
