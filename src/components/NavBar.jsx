c22c0b10f457a2c628f73bc59844c2363a4256aa
Parvez141300 <parvezhossain744471@gmail.com>
2025-07-22 02:35:51 +0600

    created a notification system for announcement

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,11 +4,26 @@ import { Bounce, toast } from "react-toastify";
 import useAuth from "../hooks/useAuth";
 import CodeStackLogo from "./CodeStackLogo";
 import { IoNotificationsCircleSharp } from "react-icons/io5";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../hooks/useAxiosSecure";
 
 const NavBar = () => {
   const { user, loading, logOut } = useAuth();
   const [toggle, setToggle] = useState(false);
   const dropDownRef = useRef(null);
+  const axiosSecure = useAxiosSecure();
+
+  // fetch announcements
+  const { data: announcements = [] } = useQuery({
+    queryKey: ["announcement"],
+    enabled: !!user?.email,
+    queryFn: async () => {
+      const res = await axiosSecure("/announcements");
+      return res.data;
+    },
+  });
+
+  console.log(announcements);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -93,7 +108,11 @@ const NavBar = () => {
     <div className="navbar w-11/12 mx-auto px-0">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden px-0">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost lg:hidden px-0"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -128,7 +147,9 @@ const NavBar = () => {
         {/* notification icon */}
         <div className="relative">
           <IoNotificationsCircleSharp size={35} />
-          <p className="absolute -top-3">0</p>
+          <p className="absolute -top-3 text-white font-bold">
+            {announcements ? announcements.length : 0}
+          </p>
         </div>
 
         {loading ? (
@@ -149,7 +170,9 @@ const NavBar = () => {
               {toggle && (
                 <div className="bg-[#0B203A] absolute -ml-24 top-0 mt-12 z-50 text-white px-5 py-2 flex flex-col">
                   <h2 className="text-nowrap">User: {user?.displayName}</h2>
-                  <Link to={'/dashboard'} className="hover:bg-[#0d233d71]">Dashboard</Link>
+                  <Link to={"/dashboard"} className="hover:bg-[#0d233d71]">
+                    Dashboard
+                  </Link>
                   <button
                     className="cursor-pointer text-start hover:bg-[#0d233d71]"
                     onClick={handleLogout}
